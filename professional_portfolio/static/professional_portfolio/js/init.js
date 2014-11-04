d3a80cf2d067c1bfb8ae2aed665c66e0ebeace29
jesseelfalan <jeselfalan@gmail.com>
2014-11-03 22:56:27 -0800

    shadowbox gird

--- professional_portfolio/static/professional_portfolio/js/init.js
+++ professional_portfolio/static/professional_portfolio/js/init.js
@@ -3,60 +3,72 @@
  *Initialization of javascript plugins
  *
  */
-$(document).ready(function(){
-    $(function(){
-        $( "#tabs" ).tabs();
-   });
+$(document).ready(function () {
+    $(function () {
+        $("#tabs").tabs();
+    });
 });
 
 /***********************/
 //debug script
-  var debugInput = document.querySelector("input");
-    function updateDebugState() {
-        document.body.classList.toggle('debug-on', debugInput.checked);
-    }
-    debugInput.addEventListener("click", updateDebugState);
-    updateDebugState();
+var debugInput = document.querySelector("input");
+function updateDebugState() {
+    document.body.classList.toggle('debug-on', debugInput.checked);
+}
+debugInput.addEventListener("click", updateDebugState);
+updateDebugState();
 
 /***************************
  * parallax features
  */
-            $(function(){
-			$.stellar({
-				horizontalScrolling: false,
-				verticalOffset: -5
-			});
-		});
+$(function () {
+    $.stellar({
+        horizontalScrolling: false,
+        verticalOffset: -5
+    });
+});
 
 /*************************/
 //animated scrolling
-	smoothScroll.init({
-				speed: 1000,
-				easing: 'easeInOutCubic',
-				offset: 0,
-				updateURL: true,
-				callbackBefore: function ( toggle, anchor ) {},
-				callbackAfter: function ( toggle, anchor ) {}
-			});
+smoothScroll.init({
+    speed: 1000,
+    easing: 'easeInOutCubic',
+    offset: 0,
+    updateURL: true,
+    callbackBefore: function (toggle, anchor) {
+    },
+    callbackAfter: function (toggle, anchor) {
+    }
+});
 
 /************************
  * picture gallery
  */
 
-           Shadowbox.init({
-    handleOversize: "drag",
-    modal: true
+Shadowbox.init({
+    handleOversize: "resize",
+    modal: true,
+//    counterType:"skip",
+    continuous:true,
+    displayNav: true,
+    displayCounter:true,
+    enableKeys:true,
+    viewportPadding:50
+//    initialHeight: "100px",
+//    initialWidth:"100px"
+
+
 });
 
 /**************************
  * blinker
  */
-            function blink(selector){
-$(selector).fadeOut('slow', function(){
-    $(this).fadeIn('slow', function(){
-        blink(this);
+function blink(selector) {
+    $(selector).fadeOut('slow', function () {
+        $(this).fadeIn('slow', function () {
+            blink(this);
+        });
     });
-});
 }
 
 blink('#greeting_box');
