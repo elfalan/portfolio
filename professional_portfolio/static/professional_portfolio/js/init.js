b04376820ddf384c1600bc36c7ea9ccced8e152c
jesseelfalan <jeselfalan@gmail.com>
2014-11-07 22:46:57 -0800

    working parallax with site reorg

--- professional_portfolio/static/professional_portfolio/js/init.js
+++ professional_portfolio/static/professional_portfolio/js/init.js
@@ -63,40 +63,40 @@ $(document).ready(function () {
 //        verticalOffset: -5
 //    });
 //});
-// Set scrolling to be in either one or both directions
+//Set scrolling to be in either one or both directions
 
-//$.stellar({
-//  horizontalScrolling: false,
-//  verticalScrolling: false,
-//
-//  // Set the global alignment offsets
-//  horizontalOffset: 0,
-//  verticalOffset: 0,
-//
-//  // Refreshes parallax content on window load and resize
-////  responsive: true,
-//    responsive:true,
-//  // Select which property is used to calculate scroll.
-//  // Choose 'scroll', 'position', 'margin' or 'transform',
-//  // or write your own 'scrollProperty' plugin.
-//  scrollProperty: 'scroll',
-//
-//  // Select which property is used to position elements.
-//  // Choose between 'position' or 'transform',
-//  // or write your own 'positionProperty' plugin.
-//  positionProperty: 'position',
-//
-//  // Enable or disable the two types of parallax
-//  parallaxBackgrounds: true,
-//  parallaxElements: true,
-//
-//  // Hide parallax elements that move outside the viewport
-//  hideDistantElements: false,
-//
-//  // Customise how elements are shown and hidden
-//  hideElement: function($elem) { $elem.hide(); },
-//  showElement: function($elem) { $elem.show(); }
-//});
+$.stellar({
+  horizontalScrolling: false,
+  verticalScrolling: true,
+
+  // Set the global alignment offsets
+  horizontalOffset: 0,
+  verticalOffset: 0,
+
+  // Refreshes parallax content on window load and resize
+//  responsive: true,
+    responsive:true,
+  // Select which property is used to calculate scroll.
+  // Choose 'scroll', 'position', 'margin' or 'transform',
+  // or write your own 'scrollProperty' plugin.
+  scrollProperty: 'scroll',
+
+  // Select which property is used to position elements.
+  // Choose between 'position' or 'transform',
+  // or write your own 'positionProperty' plugin.
+  positionProperty: 'position',
+
+  // Enable or disable the two types of parallax
+  parallaxBackgrounds: true,
+  parallaxElements: true,
+
+  // Hide parallax elements that move outside the viewport
+  hideDistantElements: false,
+
+  // Customise how elements are shown and hidden
+  hideElement: function($elem) { $elem.hide(); },
+  showElement: function($elem) { $elem.show(); }
+});
 /*************************/
 //animated scrolling
 smoothScroll.init({
